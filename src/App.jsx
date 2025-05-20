c2a33ee954ec33ffa326071fe39bda5eb4c447f6
Mohamed <mohamedkondombo135@gmail.com>
2025-05-20 01:43:20 -0400

    init audiophile

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import FeatureProduct from "./sections/FeatureProduct";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Products from "./sections/Products";
+
+const App = () => {
+  return (
+    <main className="bg-red-800">
+      <Header />
+      <Hero />
+      <Products />
+      <FeatureProduct />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
